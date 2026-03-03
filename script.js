69439931c7484347f3f3ba57da9c1f430493ff1c
ashutoshdash-eva <ashutoshdash@evawinoptimize.com>
2026-03-03 10:35:03 +0530

    Add initial project setup with Three.js and Vite

---
+++ script.js
@@ -0,0 +1,20 @@
+import * as THREE from 'three';
+// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
+camera.position.set(0,18,15);
+const renderer = new THREE.WebGLRenderer({antialias:true});
+renderer.setSize(window.innerWidth/window.innerHeight);
+renderer.shadowMap.enabled = true;
+document.appendChild(renderer.domElement);
+// const controls = new OrbitControls(camera,renderer.domElement);
+// controls.enableDamping  = true;
+// controls.target.set(2.5,2.5,0);
+// controls.update();
+const ambientLight = new THREE.AmbientLight(0xffffff,0.6);
+scene.add(ambientLight);
+const sunLight = new THREE.DirectionalLight(0xffffff,1);
+sunLight.position.set(10,20,10);
+sunLight.castShadow = true;
+scene.add(sunLight);
