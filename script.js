e61d2045326239974606da8bdf829e2267566f46
ashutoshdash-eva <ashutoshdash@evawinoptimize.com>
2026-03-09 19:04:32 +0530

    added texture

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 import * as THREE from 'three';
+import { texture } from 'three/tsl';
+import { OrbitControls } from 'three/examples/jsm/Addons.js';  
 
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0, 15, 15);
+camera.position.set(0, 7.5, 15);
 camera.lookAt(0, 0, 0);
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls(camera,renderer.domElement);
+controls.enableDamping = true;
 
 const light = new THREE.DirectionalLight(0xffffff, 2.5);
 light.position.set(10, 20, 10);
@@ -25,18 +30,60 @@ let score = 0;
 let gameOver = false;
 
 const boxGeo = new THREE.BoxGeometry(0.9, 0.9, 0.9);
-const snakeMat = new THREE.MeshPhysicalMaterial({ color: '#00ffc3', roughness: 0.2, metalness: 0.1 });
-const foodMat = new THREE.MeshStandardMaterial({ color: '#ff0055', emissive: '#ff0055', emissiveIntensity: 0.5 }); 
-const boardGeo = new THREE.PlaneGeometry(gridSize, gridSize);
-const boardMat = new THREE.MeshPhysicalMaterial({ color: '#1a1a1a', side: THREE.DoubleSide, roughness: 0.8, metalness: 0.5 }); 
+// const snakeMat = new THREE.MeshPhysicalMaterial({ color: '#00ffc3', roughness: 0.2, metalness: 0.1 });
+// const foodMat = new THREE.MeshStandardMaterial({ color: '#ff0055', emissive: '#ff0055', emissiveIntensity: 0.5 }); 
+const boardGeo = new THREE.PlaneGeometry(gridSize, gridSize, 100, 100);
+// const boardMat = new THREE.MeshPhysicalMaterial({ color: '#1a1a1a', side: THREE.DoubleSide, roughness: 0.8, metalness: 0.5 }); 
+
+const foodGeo = new THREE.SphereGeometry(0.45, 32, 32);
+
+const textureLoader = new THREE.TextureLoader();
+
+const colorMap = textureLoader.load('textures/Color.png');
+colorMap.colorSpace = THREE.SRGBColorSpace; // Crucial: Keeps the colors from looking washed out
+
+const normalMap = textureLoader.load('textures/NormalGL.png');
+const roughnessMap = textureLoader.load('textures/Roughness.png');
+const displacementMap = textureLoader.load('textures/Displacement.png');
+const aoMap = textureLoader.load('textures/AmbientOcclusion.png');
+
+const boardMat = new THREE.MeshStandardMaterial({
+    map: colorMap,
+    normalMap: normalMap,
+    roughnessMap: roughnessMap,
+    aoMap: aoMap,
+    displacementMap: displacementMap,
+    displacementScale: 1,
+    side: THREE.DoubleSide
+})
+
+const snakeMat = new THREE.MeshStandardMaterial({
+    map: textureLoader.load("./textures/snake/color.png"),
+    colorSpace: THREE.SRGBColorSpace,
+    normalMap: textureLoader.load("./textures/snake/normal.png"),
+    roughnessMap: textureLoader.load("./textures/snake/roughness.png"),
+    aoMap: textureLoader.load("./textures/snake/ao.png"),
+    displacementMap: textureLoader.load("./textures/snake/displacement.png"),
+    displacementScale: 0.01,
+})
+
+const foodMat = new THREE.MeshStandardMaterial({ 
+    map: textureLoader.load("./textures/food/color.png"),
+    colorSpace: THREE.SRGBColorSpace,
+    normalMap: textureLoader.load("./textures/food/normal.png"),
+    roughnessMap: textureLoader.load("./textures/food/roughness.png"),
+    aoMap: textureLoader.load("./textures/food/ao.png"),
+});
 
 const board = new THREE.Mesh(boardGeo, boardMat);
-board.rotation.x = Math.PI / 2;
-board.position.y = -0.5;
+board.rotation.x = -Math.PI / 2;
+board.position.z = -0.5;
+board.position.y = -1.5;
 scene.add(board);
 
 const grid = new THREE.GridHelper(gridSize, gridSize, 0x444444, 0x222222);
-grid.position.y = -0.49; 
+grid.position.y = -1; 
+grid.position.z = -0.5;
 scene.add(grid);
 
 const scoreElement = document.getElementById('score');
@@ -83,7 +130,7 @@ function spawnFood() {
     }
 
     if (!food) {
-        food = new THREE.Mesh(boxGeo, foodMat);
+        food = new THREE.Mesh(foodGeo, foodMat);
         scene.add(food);
     }
     food.position.set(rx, 0, rz);
@@ -178,7 +225,9 @@ function animate(currentTime) {
             lastMoveTime = currentTime;
         }
     }
+    controls.update();
     renderer.render(scene, camera);
+    
 }
 requestAnimationFrame(animate);
 
